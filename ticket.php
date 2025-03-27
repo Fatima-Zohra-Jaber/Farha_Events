86e766a4c8f904ad9fb24a15216188d9669299ef
Fatima-Zohra-Jaber <jaber.fatiima.zohra@gmail.com>
2025-03-27 15:58:13 +0100

    Ticket

--- ticket.php
+++ ticket.php
@@ -1,6 +1,53 @@
 <?php
     require('config.php');
+    if (!empty($_GET['id'])) {
+    $id = $_GET['id'];
+    $sql = "SELECT ev.eventTitle, ev.TariffNormal,ev.TariffReduit,ed.dateEvent, 
+                    ed.timeEvent, r.qteBilletsNormal,r.qteBilletsReduit,ed.numSalle,
+                    b.billetId,b.typeBillet,b.placeNum              
+            FROM reservation r JOIN edition ed on r.editionId = ed.editionId
+            JOIN evenement ev  ON ev.eventId = ed.eventId
+            JOIN billet b ON r.idReservation = b.idReservation
+            WHERE r.idReservation = :id";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':id', $id, PDO::PARAM_INT);
+    $stmt->execute();
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    // var_dump($result);
+    if (!$result) {
+        header("Location: index.php");
+        exit();
+    }
+    // $formattedDateTime = $dateTime->format('d/m/Y à H:i');
     
+
+    $dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $result[0]['dateEvent'] . ' ' . $result[0]['timeEvent']);
+
+    $formatter = new IntlDateFormatter(
+        'fr_FR',
+        IntlDateFormatter::FULL,
+        IntlDateFormatter::SHORT,
+        'Europe/Paris',
+        IntlDateFormatter::GREGORIAN,
+        "EEEE d MMMM yyyy 'À' HH'H'mm"
+    );
+    
+//     $dateTime = DateTime::createFromFormat('Y-m-d H:i:s', $result[0]['dateEvent'] . ' ' . $result[0]['timeEvent']);
+
+// setlocale(LC_TIME, 'fr_FR.UTF-8', 'fr_FR', 'fr');
+
+// // Formater la date en français avec strftime() n'est plus possible, on utilise format()
+// $formattedDateTime = strftime('%A %d %B %Y À %HH%M', strtotime($dateTime->format('Y-m-d H:i:s')));
+
+// // Afficher en majuscules
+// echo mb_strtoupper($formattedDateTime, 'UTF-8');
+
+    
+    
+
+
+
+
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -11,18 +58,19 @@
     <link rel="stylesheet" href="ticket.css">
 </head>
 <body>
-
+    <?php foreach($result as $billet): ?>
     <div class="ticket">
         <div class="left-section">
-            <div class="ticket-number">#0123456789</div>
+            <div class="ticket-number">#<?=$billet['billetId']?></div>
             <div class="ticket-number">Numéro de ticket:</div>
             <div class="circle"></div>
         </div>
 
         <div class="middle-section">
             <div class="titre">
-                <h1>SOIRÉE MAROCAINE</h1>
-                <div class="date">SAMEDI 27 JANVIER 2024 À 20H00</div>
+                <h1><?=$billet['eventTitle']?></h1>
+                <div class="date"><?= mb_strtoupper($formatter->format($dateTime), 'UTF-8')?></div>
+            <!-- SAMEDI 27 JANVIER 2024 À 20H00 -->
             </div>
             
             <div class="info">
@@ -37,7 +85,7 @@
                             <p class="bold">Tarif :</p><span>MAD 150,00</span> 
                         </div>
                         <div class="type">
-                            <p class="bold">Type :</p><span> Tarif réduit</span> 
+                            <p class="bold">Type :</p><span> Tarif <?=$billet['typeBillet']?></span> 
                         </div>
                     </div>
                     <div class="adresse">
@@ -54,17 +102,28 @@
             <div class="rightInfo">
                 <div class="seat-info">
                     <div class="label">PLACE</div>
-                    <div class="value">67</div>
+                    <div class="value"><?=$billet['placeNum']?></div>
                 </div>
                 <div class="seat-info">
                     <div class="label">SALLE</div>
-                    <div class="value">02</div>
+                    <div class="value">0<?=$billet['numSalle']?></div>
                 </div>
             </div>
-            
+            <div class="cercleBlock">
+                <div class="cercleRight"></div>
+                <div class="cercleRight"></div>
+                <div class="cercleRight"></div>
+                <div class="cercleRight"></div>
+                <div class="cercleRight"></div>
+            </div>
         </div>
     </div>
+    <?php endforeach; ?>
 
 </body>
 </html>
-
+<?php
+    } else {
+        header("Location: index.php");
+        exit();
+    }
